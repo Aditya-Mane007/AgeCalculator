0bd960bd9ce088d70717f0b9a76dbaf2d6b6c6c6
Aditya <maneaditya006@gmail.com>
2023-05-21 15:15:07 +0530

    Better code

--- app.js
+++ app.js
@@ -8,61 +8,116 @@ const submit = document.querySelector(".submit-btn")
 
 
 submit.addEventListener('click',() => {
-
+  // Days
+  // Checking for if date is over 31
   if (day.value > 31) {
     day.parentElement.childNodes[1].style.color = "hsl(0, 100%, 67%)"
     const error_msg = day.parentElement.childNodes[5]
     error_msg.innerText = "Must be valid value"
     error_msg.style.color = "red"
   } else if (!day.value) {
+    // Check for if date is !empty
     day.parentElement.childNodes[1].style.color = "red"
     const error_msg = day.parentElement.childNodes[5]
     error_msg.innerText = "This field is required"
     error_msg.style.color = "red"
   } else {
+    // Everything is correct 
     day.parentElement.childNodes[1].style.color = "hsl(0, 1%, 44%)"
     const error_msg = day.parentElement.childNodes[5]
     error_msg.innerText = ""
 
+    const days = document.querySelector(".day-output")
+    days.childNodes[0].innerText = calculateDate(new Date().getDate(),day.value)
   }
+
+  // MONTHS
+  // Checking for if month is over 12
   if (month.value > 13) {
     month.parentElement.childNodes[1].style.color = "red"
     const error_msg = month.parentElement.childNodes[5]
     error_msg.innerText = "Must be valid value"
     error_msg.style.color = "red"
   } else if (!month.value) {
+    // checking for if month value is !empty
     month.parentElement.childNodes[1].style.color = "red"
     const error_msg = month.parentElement.childNodes[5]
     error_msg.innerText = "This field is required"
     error_msg.style.color = "red"
   } else {
+    // Everything is correct
     month.parentElement.childNodes[1].style.color = "hsl(0, 1%, 44%)"
     const error_msg = month.parentElement.childNodes[5]
     error_msg.innerText = ""
+
+    const months = document.querySelector(".month-output")
+    months.childNodes[1].innerText = calculateMonth(new Date().getMonth(),month.value)
   }
 
+  // Years
+  // Checking for if year value is !empty
   if (!year.value) {
     year.parentElement.childNodes[1].style.color = "red"
     const error_msg = year.parentElement.childNodes[5]
     error_msg.innerText = "This field is required"
     error_msg.style.color = "red"
-  } else {
+  } else if (year.value > new Date().getFullYear()) {
+    // Checking for no future year value
+    year.parentElement.childNodes[1].style.color = "red"
+    const error_msg = year.parentElement.childNodes[5]
+    error_msg.innerText = "No future years"
+    error_msg.style.color = "red"
+  }
+  else {
+    // Everything is correct
     year.parentElement.childNodes[1].style.color = "hsl(0, 1%, 44%)"
     const error_msg = year.parentElement.childNodes[5]
     error_msg.innerText = ""
+    const years = document.querySelector(".year-output")
+    years.childNodes[0].innerText = calculateYear(new Date().getFullYear(),year.value)
+  }
+})
+
+let finalYear
+const calculateDate = (currentDate,birthDate) => {
+  let finalDate
+  if (currentDate < birthDate) {
+    const res = Math.abs(currentDate - birthDate)
+    finalDate = 30 - res
+    finalYear++
+  } else {
+    finalDate = currentDate - birthDate
   }
 
-  const years = document.querySelector(".year-output")
-  years.childNodes[0].innerText = new Date().getFullYear() - year.value
+  return finalDate
+}
 
+const calculateMonth = (currentMonth,birthMonth) => {
+  let finalMonth
 
-  const months = document.querySelector(".month-output")
-  const MonthValue = months.childNodes[1].innerText = new Date().getMonth() - month.value
+  finalMonth = currentMonth - birthMonth
+  if (finalMonth == 0) {
+    const res = 12 + 1
+    finalMonth = res
+    finalYear++
+  }
+  if (currentMonth < birthMonth) {
+    const res = Math.abs(currentMonth - birthMonth)
+    finalMonth = 12 - res
+    finalYear++
+  }
 
-  const days = document.querySelector(".day-output")
+  return finalMonth
 
-  const finalDate = Math.abs(new Date().getDate() - day.value)
+}
 
-  days.childNodes[0].innerText = 30 - finalDate
-})
-console.log(new Date().getUTCMonth())
+const calculateYear = (currentYear,birthYear) => {
+
+  res = currentYear - birthYear
+  if (finalYear > 1) {
+    finalYear = res - 1
+  } else {
+    finalYear = res
+  }
+  return finalYear
+}
